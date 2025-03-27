f91d3adff7ea49bd48469dbf832e07566582d5c8
luiggix <luiggix@gmail.com>
2025-03-26 18:11:45 -0600

    osys.py, physpar.py, 01_flujo_1D.ipynb

---
+++ src/xmf6/__init__.py
@@ -0,0 +1,7 @@
+# Módulos que se van a integrar en xmf6
+from xmf6.mesh import MeshDis
+from xmf6.osys import nice_print, OFiles, OSPar
+from xmf6.physpar import PhysPar
+from xmf6.vis import plot, scatter
+from xmf6.tdis import TDis
+import xmf6.df_implicit 
