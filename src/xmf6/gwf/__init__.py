538fcdea5274ae3dbf0f404574958b3cd6e89322
luiggix <luiggix@gmail.com>
2025-07-07 11:09:29 -0600

    set_components, set_packages

--- src/xmf6/gwf/__init__.py
+++ src/xmf6/gwf/__init__.py
@@ -1,5 +1,5 @@
 # Desde el .[nombre del archivo] importa [lista de funciones] 
-from .gwf import set_components, set_components, get_head, get_specific_discharge
+from .gwf import set_components, set_packages, get_head, get_specific_discharge
 
 # que nombres se exportan si se usa from macti.visual import * 
-__all__ = ["set_components", "set_components", "get_head", "get_specific_discharge"]
+__all__ = ["set_components", "set_packages", "get_head", "get_specific_discharge"]
